0c4b97acc72387ca5d35a12e20cb942f9b3a61e1
Sleyder Zuleta <sleyderzm1@gmail.com>
2023-03-25 20:39:10 -0500

    generate users model with a uniq user by default and a structure for the home controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    #todo: create session logic
+    @current_user ||= User.first #return default user
+    @current_user
+  end
 end
